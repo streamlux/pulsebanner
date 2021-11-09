cb0dc381a71efdd8f1ec1d47127082eb57195e73
alexweininger <alex.weininger@live.com>
2021-11-09 02:28:08 -0500

    Add basic SEO

--- apps/next/components/layout.tsx
+++ apps/next/components/layout.tsx
@@ -5,18 +5,20 @@ import Footer from './footer';
 
 export default function Layout({ children }) {
     return (
-        <Flex direction="column" as={chakra.div} minH="100vh">
-            <Box as={chakra.header}>
-                <Header />
-            </Box>
-            <Flex as={chakra.main} flex="1" px={['2', '8', '16', '36']} w="100vw">
-                <Box w="full" pt="20">
-                    {children}
+        <>
+            <Flex direction="column" as={chakra.div} minH="100vh">
+                <Box as={chakra.header}>
+                    <Header />
+                </Box>
+                <Flex as={chakra.main} flex="1" px={['2', '8', '16', '36']} w="100vw">
+                    <Box w="full" pt="20">
+                        {children}
+                    </Box>
+                </Flex>
+                <Box as={chakra.footer}>
+                    <Footer />
                 </Box>
             </Flex>
-            <Box as={chakra.footer}>
-                <Footer />
-            </Box>
-        </Flex>
+        </>
     );
 }
--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -7,6 +7,7 @@ import Layout from '../components/layout';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import favicon from '../public/favicon.png';
+import { DefaultSeo } from 'next-seo';
 
 // Use of the <SessionProvider> is now mandatory to allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
@@ -19,6 +20,7 @@ export default function App({ Component, pageProps }) {
                 <link rel="shortcut icon" sizes="192x192" href={favicon.src} />
                 <link rel="apple-touch-icon" href={favicon.src} />
             </Head>
+            <DefaultSeo title="PulseBanner - Twitter live banner for Twitch" description="Easily attract more viewers to your stream from Twitch." />
             <SessionProvider session={pageProps.session}>
                 <ChakraProvider theme={theme}>
                     {/* {router.pathname === '/' ? (
