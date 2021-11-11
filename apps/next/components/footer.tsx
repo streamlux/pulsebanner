3d52f0377598e8309364b5689c558953a868bd2c
alexweininger <alex.weininger@live.com>
2021-11-11 00:43:54 -0500

    Edit footer

--- apps/next/components/footer.tsx
+++ apps/next/components/footer.tsx
@@ -1,7 +1,6 @@
 import NextLink from 'next/link';
 import { Center, Link } from '@chakra-ui/react';
 import styles from './footer.module.css';
-import packageJSON from '../../../package.json';
 import React from 'react';
 
 export default function Footer() {
@@ -10,30 +9,15 @@ export default function Footer() {
             <Center>
                 <ul className={styles.navItems}>
                     <li className={styles.navItem}>
-                        <Link href="https://next-auth.js.org">Documentation</Link>
+                        <Link href="https://twitter.com/pulsebanner">Twitter</Link>
                     </li>
                     <li className={styles.navItem}>
-                        <Link href="https://www.npmjs.com/package/next-auth">NPM</Link>
-                    </li>
-                    <li className={styles.navItem}>
-                        <Link href="https://github.com/nextauthjs/next-auth-example">GitHub</Link>
+                        <Link href="https://github.com/streamlux/pulsebanner">GitHub</Link>
                     </li>
                     <li className={styles.navItem}>
                         <NextLink href="/policy">
                             <Link>Policy</Link>
                         </NextLink>
-                    </li>
-                    <li className={styles.navItem}>
-                        <em>next-auth@{packageJSON.dependencies['next-auth']}</em>
-                    </li>
-                    <li className={styles.navItem}>
-                        <em>next@{packageJSON.dependencies['next']}</em>
-                    </li>
-                    <li className={styles.navItem}>
-                        <em>{`react@${packageJSON.dependencies['react']}`}</em>
-                    </li>
-                    <li className={styles.navItem}>
-                        <em>@chakra-ui/react@{packageJSON.dependencies['@chakra-ui/react']}</em>
                     </li>
                 </ul>
             </Center>
