6fe1962b967ededc6f9ca4c976d3887e74c2cbab
alexweininger <alex.weininger@live.com>
2021-11-19 07:43:24 -0500

    Fix watermark

--- libs/remotion/components/src/lib/Watermark.tsx
+++ libs/remotion/components/src/lib/Watermark.tsx
@@ -7,7 +7,7 @@ export const Watermark: React.FC = () => (
         <div style={{ position: 'absolute', right: '0', top: '0', background: 'rgba(255, 255, 255, 0.50)', borderRadius: '0 0 0 5px', height: '32px' }}>
             <div style={{ fontSize: '18px', fontFamily: 'Inter', display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '2px' }}>
                 <span style={{ paddingRight: '3px' }}>
-                    <Img height={24} width={24} src={'http://pulsebanner.com/favicon.webp'} />
+                    <Img height={24} width={24} src={'http://pulsebanner.com/logo.webp'} />
                 </span>{' '}
                 <span style={{ color: 'black' }}>PulseBanner.com</span>
             </div>
