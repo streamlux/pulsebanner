83fd9ded41426ea5d70107b3c9578e7109246c84
alexweininger <alex.weininger@live.com>
2021-11-19 17:40:43 -0500

    Move watermark to bottom center

--- libs/remotion/components/src/lib/Watermark.tsx
+++ libs/remotion/components/src/lib/Watermark.tsx
@@ -4,7 +4,17 @@ import './foregrounds/fonts.module.css';
 
 export const Watermark: React.FC = () => (
     <AbsoluteFill style={{ height: '100%', width: '100%' }}>
-        <div style={{ position: 'absolute', right: '0', top: '0', background: 'rgba(255, 255, 255, 0.50)', borderRadius: '0 0 0 5px', height: '32px' }}>
+        <div
+            style={{
+                position: 'absolute',
+                transform: 'translate(50%, 0%)',
+                right: '50%',
+                bottom: '0',
+                background: 'rgba(255, 255, 255, 0.50)',
+                borderRadius: '5px 5px 0 0',
+                height: '32px',
+            }}
+        >
             <div style={{ fontSize: '18px', fontFamily: 'Inter', display: 'flex', flexDirection: 'row', alignItems: 'center', padding: '2px' }}>
                 <span style={{ paddingRight: '3px' }}>
                     <Img height={24} width={24} src={'http://pulsebanner.com/logo.webp'} />
