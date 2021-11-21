3f722fd8ab79b7b5be531053d992a1a17dbdd0ea
alexweininger <alex.weininger@live.com>
2021-11-21 17:48:18 -0500

    Fixup

--- apps/next/components/Card.tsx
+++ apps/next/components/Card.tsx
@@ -1,7 +1,7 @@
 import { Box, BoxProps, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 
-export const Card: React.FC<{ props: BoxProps }> = ({ children, props }) => {
+export const Card: React.FC<{ props?: BoxProps }> = ({ children, props }) => {
     const styles = useColorModeValue<BoxProps>(
         {
             border: '1px solid',
@@ -13,7 +13,7 @@ export const Card: React.FC<{ props: BoxProps }> = ({ children, props }) => {
     );
 
     return (
-        <Box rounded="md" p="4" experimental_spaceY="8" {...styles} {...props}>
+        <Box rounded="md" p="4" experimental_spaceY="8" {...styles} {...(props ?? {})}>
             {children}
         </Box>
     );
