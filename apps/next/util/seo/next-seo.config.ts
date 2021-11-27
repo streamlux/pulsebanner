9eb751bc5d0c3505d0398a7d7bdb32cba42f4025
alexweininger <alex.weininger@live.com>
2021-11-27 00:52:56 -0500

    Fix typo in website title

--- apps/next/util/seo/next-seo.config.ts
+++ apps/next/util/seo/next-seo.config.ts
@@ -6,7 +6,7 @@ const seoOptions: NextSeoProps = {
 
     // Basic information https://github.com/garmeeh/next-seo#add-seo-to-page
     title: "PulseBanner - Twitter live banner for Twitch",
-    description: "Easily attract more viewers to your stream from Twitch.",
+    description: "Easily attract more viewers to your stream from Twitter.",
 
     // https://github.com/garmeeh/next-seo#open-graph
     openGraph: {
