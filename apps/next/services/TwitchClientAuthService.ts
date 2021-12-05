0ff1537ddd871490fe0521caa6a9782711113607
alexweininger <alex.weininger@live.com>
2021-12-04 22:19:51 -0500

    Auto refresh client token

--- apps/next/services/TwitchClientAuthService.ts
+++ apps/next/services/TwitchClientAuthService.ts
@@ -1,16 +1,27 @@
 import { AxiosInstance } from "axios";
-import { ClientCredentialsAuthProvider } from "twitch-auth";
+import { AccessToken, ClientCredentialsAuthProvider } from "twitch-auth";
 
 export class TwitchClientAuthService {
     private static authProvider: ClientCredentialsAuthProvider | undefined;
 
-    public static getAuthProvider(): ClientCredentialsAuthProvider {
+    public static _getAuthProvider(): ClientCredentialsAuthProvider {
         this.authProvider ||= new ClientCredentialsAuthProvider(process.env.TWITCH_CLIENT_ID, process.env.TWITCH_CLIENT_SECRET);
         return this.authProvider;
     }
 
+    public static async getAccessToken(): Promise<AccessToken> {
+        const token = await this._getAuthProvider().getAccessToken();
+
+        if (token.isExpired) {
+            return this._getAuthProvider().refresh();
+        };
+
+        return token;
+    }
+
     public static async authAxios(axios: AxiosInstance): Promise<AxiosInstance> {
-        const accessToken = (await this.getAuthProvider().getAccessToken()).accessToken;
+        const accessToken = (await this.getAccessToken()).accessToken;
+
         axios.defaults.headers = {
             'Client-ID': process.env.TWITCH_CLIENT_ID,
             Authorization: `Bearer ${accessToken}`,
