a1848b7d52b6287c97e1cc247bf13d921c75ff8d
alexweininger <alex.weininger@live.com>
2021-12-04 22:22:32 -0500

    Make method private

--- apps/next/services/TwitchClientAuthService.ts
+++ apps/next/services/TwitchClientAuthService.ts
@@ -4,7 +4,7 @@ import { AccessToken, ClientCredentialsAuthProvider } from "twitch-auth";
 export class TwitchClientAuthService {
     private static authProvider: ClientCredentialsAuthProvider | undefined;
 
-    public static _getAuthProvider(): ClientCredentialsAuthProvider {
+    private static _getAuthProvider(): ClientCredentialsAuthProvider {
         this.authProvider ||= new ClientCredentialsAuthProvider(process.env.TWITCH_CLIENT_ID, process.env.TWITCH_CLIENT_SECRET);
         return this.authProvider;
     }
