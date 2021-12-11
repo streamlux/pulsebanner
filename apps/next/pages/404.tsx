4d8b89044161562d94cf181d5fa3adc0b85fa495
alexweininger <alex.weininger@live.com>
2021-12-10 20:32:25 -0500

    Create custom 404 page

---
+++ apps/next/pages/404.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Box, Center, Text, Container, Heading, Image, Link, VStack, Wrap, WrapItem, Button } from '@chakra-ui/react';
+import type { NextPage } from 'next';
+import NextLink from 'next/link';
+import { discordLink } from '@app/util/constants';
+import { FaDiscord } from 'react-icons/fa';
+
+const imgSrc = 'https://pb-static.sfo3.cdn.digitaloceanspaces.com/404.webp';
+
+const Page: NextPage = () => {
+    return (
+        <Container maxW="container.lg">
+            <Center>
+                <Box experimental_spaceY={4}>
+                    <Heading as="p">
+                        <Text as="span" color="red.300">
+                            404
+                        </Text>{' '}
+                        Page not found
+                    </Heading>
+                    <Image src={imgSrc} alt="" />
+
+                    <Center>
+                        <VStack spacing={4}>
+                            <Box experimental_spaceY={2}>
+                                <Heading fontSize="lg">Try one of these pages 👇</Heading>
+                                <Center id="nav-links" fontSize="lg">
+                                    <Wrap spacing={['2', '4', '8', '10']}>
+                                        <WrapItem>
+                                            <NextLink href="/banner" passHref>
+                                                <Link>Banner</Link>
+                                            </NextLink>
+                                        </WrapItem>
+                                        <WrapItem>
+                                            <NextLink href="/pricing" passHref>
+                                                <Link>Pricing</Link>
+                                            </NextLink>
+                                        </WrapItem>
+                                    </Wrap>
+                                </Center>
+                            </Box>
+                            <Heading fontSize="lg">Or complain to the developers 🤭</Heading>
+                            <Link isExternal href={discordLink}>
+                                <Button as="a" size="sm" colorScheme="gray" rightIcon={<FaDiscord />}>
+                                    Join our Discord
+                                </Button>
+                            </Link>
+                        </VStack>
+                    </Center>
+                </Box>
+            </Center>
+        </Container>
+    );
+};
+
+export default Page;
