46af126272c0c0102f14940c229b0ea6ea23db02
AndrewJLang <andrewespn@hotmail.com>
2021-12-13 21:10:30 -0800

    db migration for new table

---
+++ prisma/migrations/20211214051017_username_feature_table/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "username" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "streamName" TEXT NOT NULL,
+
+    CONSTRAINT "username_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "original_name" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "originalName" TEXT NOT NULL,
+
+    CONSTRAINT "original_name_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "twitter_username_user_id_unique" ON "username"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "twitter_original_username_user_id_unique" ON "original_name"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "username" ADD CONSTRAINT "username_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "original_name" ADD CONSTRAINT "original_name_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
