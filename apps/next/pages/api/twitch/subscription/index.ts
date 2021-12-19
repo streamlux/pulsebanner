df9d0409921e6bfececee2976e7ca3d29d49fe12
alexweininger <alex.weininger@live.com>
2021-12-19 16:39:06 -0500

    List subscriptions hotfix

--- apps/next/pages/api/twitch/subscription/index.ts
+++ apps/next/pages/api/twitch/subscription/index.ts
@@ -3,9 +3,9 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/react';
 import NextCors from 'nextjs-cors';
 import { Account } from '@prisma/client';
-import { GetSubscriptionsResponse } from '@app/types/twitch';
 import { twitchAxios } from '@app/util/axios';
 import { TwitchClientAuthService } from '@app/services/TwitchClientAuthService';
+import { listSubscriptions } from '@app/util/twitch/listSubscriptions';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     // Run the cors middleware
@@ -26,14 +26,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         const token = await TwitchClientAuthService.getAccessToken();
 
-        const response = await twitchAxios.get<GetSubscriptionsResponse>('/helix/eventsub/subscriptions', {
-            headers: {
-                'Client-ID': process.env.TWITCH_CLIENT_ID,
-                Authorization: `Bearer ${token.accessToken}`,
-            },
-        });
-        const allSubscriptions = response.data.data;
-
+        const allSubscriptions = await listSubscriptions();
         if (req.method === 'GET') {
             if (session.user['role'] === 'admin') {
                 res.status(200).json({ subscriptions: allSubscriptions });
