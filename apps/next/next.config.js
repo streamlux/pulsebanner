5455f5f8c8645419e90812af6487e2ed2f7395b6
alexweininger <alex.weininger@live.com>
2022-01-06 20:36:35 -0800

    Comment out redirect

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -19,6 +19,16 @@ const nextConfig = {
         // Important: return the modified config
         return config;
     },
+    // async redirects() {
+    //     return [
+    //         {
+    //             source: '/',
+    //             destination: '/banner',
+    //             permanent: true,
+    //             statusCode: 301
+    //         },
+    //     ]
+    // },
     // fix heap limit allocation failed issue with NextJS 12
     // https://github.com/vercel/next.js/issues/30330#issuecomment-952172377
     experimental: {
