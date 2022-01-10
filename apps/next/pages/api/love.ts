7ace8400811c99eb996e156cb922572bacefc4c6
alexweininger <alex.weininger@live.com>
2022-01-09 19:34:49 -0800

    Create api endpoint for getting umami script

---
+++ apps/next/pages/api/love.ts
@@ -0,0 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
+import axios from 'axios';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Run the cors middleware
+    // nextjs-cors uses the cors package, so we invite you to check the documentation https://github.com/expressjs/cors
+    await NextCors(req, res, {
+        // Options
+        // methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        methods: ['GET'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
+
+    const umami = await axios.get('https://u.pulsebanner.com/umami.js');
+    res.send(umami.data);
+}
