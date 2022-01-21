a7dc0d58c81627e86d7e5c49755953cdb26f1dde
AndrewJLang <andrewespn@hotmail.com>
2022-01-20 21:29:49 -0800

    db migration

---
+++ prisma/migrations/20220121052942_active_streams_and_past_streams_tables/migration.sql
@@ -0,0 +1,90 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ProfilePic` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ProfilePicRendered` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "ProfilePic" DROP CONSTRAINT "ProfilePic_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "ProfilePicRendered" DROP CONSTRAINT "ProfilePicRendered_user_id_fkey";
+
+-- DropTable
+DROP TABLE "ProfilePic";
+
+-- DropTable
+DROP TABLE "ProfilePicRendered";
+
+-- CreateTable
+CREATE TABLE "profile_img" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "foregroundId" TEXT NOT NULL,
+    "backgroundId" TEXT NOT NULL,
+    "backgroundProps" JSONB NOT NULL,
+    "foregroundProps" JSONB NOT NULL,
+
+    CONSTRAINT "profile_img_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rendered_profile_img" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "last_rendered" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "rendered_profile_img_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "live_streams" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "streamLink" TEXT,
+    "twitterLink" TEXT,
+    "twitchUserId" TEXT,
+    "twitchStreamId" TEXT,
+    "startTime" TIMESTAMP(3),
+
+    CONSTRAINT "live_streams_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "past_streams" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "twitchUserId" TEXT,
+    "twitchStreamId" TEXT,
+    "startTime" TIMESTAMP(3),
+    "endTime" TIMESTAMP(3),
+
+    CONSTRAINT "past_streams_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "twitter_profile_picture" ON "profile_img"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "rendered_profile_img_user_id_key" ON "rendered_profile_img"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "live_streams_user_id_key" ON "live_streams"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "past_streams_user_id_key" ON "past_streams"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "profile_img" ADD CONSTRAINT "profile_img_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "rendered_profile_img" ADD CONSTRAINT "rendered_profile_img_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "live_streams" ADD CONSTRAINT "live_streams_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "past_streams" ADD CONSTRAINT "past_streams_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
