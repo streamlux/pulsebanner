674187261e24bcaa18ed616f54bcef6430748f27
AndrewJLang <andrewespn@hotmail.com>
2022-01-20 22:54:34 -0800

    remove profile pic tables

--- prisma/migrations/20220121052942_active_streams_and_past_streams_tables/migration.sql
+++ prisma/migrations/20220121052942_active_streams_and_past_streams_tables/migration.sql
@@ -1,27 +1,4 @@
 -- CreateTable
-CREATE TABLE "profile_img" (
-    "id" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
-    "enabled" BOOLEAN NOT NULL DEFAULT false,
-    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "foregroundId" TEXT NOT NULL,
-    "backgroundId" TEXT NOT NULL,
-    "backgroundProps" JSONB NOT NULL,
-    "foregroundProps" JSONB NOT NULL,
-
-    CONSTRAINT "profile_img_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "rendered_profile_img" (
-    "id" TEXT NOT NULL,
-    "user_id" TEXT NOT NULL,
-    "last_rendered" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "rendered_profile_img_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "live_streams" (
     "id" TEXT NOT NULL,
     "user_id" TEXT NOT NULL,
@@ -47,22 +24,10 @@ CREATE TABLE "past_streams" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "twitter_profile_picture" ON "profile_img"("user_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "rendered_profile_img_user_id_key" ON "rendered_profile_img"("user_id");
-
--- CreateIndex
 CREATE UNIQUE INDEX "live_streams_user_id_key" ON "live_streams"("user_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "past_streams_user_id_key" ON "past_streams"("user_id");
-
--- AddForeignKey
-ALTER TABLE "profile_img" ADD CONSTRAINT "profile_img_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "rendered_profile_img" ADD CONSTRAINT "rendered_profile_img_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "live_streams" ADD CONSTRAINT "live_streams_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
