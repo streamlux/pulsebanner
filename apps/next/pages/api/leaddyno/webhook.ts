b7a7f740842fb67526728c69960120be9dd7c54f
AndrewJLang <andrewespn@hotmail.com>
2022-01-31 18:49:53 -0800

    add logging

--- apps/next/pages/api/leaddyno/webhook.ts
+++ apps/next/pages/api/leaddyno/webhook.ts
@@ -1,9 +1,12 @@
+import { logger } from '@app/util/logger';
 import { createApiHandler } from '@app/util/ssr/createApiHandler';
 
 const handler = createApiHandler();
 
 handler.post(async (req, res) => {
     const body = req;
-    console.log('body: ', body);
+    logger.warn('Got the body of the request: ', { info: body });
     res.status(200).send('Succes');
 });
+
+export default handler;
