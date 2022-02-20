a8a9378e9398bb99c6d18db4b3c52a9c8c02b269
AndrewJLang <andrewespn@hotmail.com>
2022-02-19 21:45:46 -0800

    code changes

--- prisma/migrations/20220220052400_customer_required_on_partner/migration.sql
+++
@@ -1,3 +0,0 @@
-
--- CreateIndex
-CREATE UNIQUE INDEX "partner_invoice_customerId_key" ON "partner_invoice"("customerId");
--- prisma/migrations/20220220053614_remove_linking_to_customer_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "partner_invoice" DROP CONSTRAINT "partner_invoice_customerId_fkey";
-
--- DropIndex
-DROP INDEX "partner_invoice_customerId_key";
