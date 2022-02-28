10d94233c6b58906400a38ade0fbf2deba617c62
alexweininger <alex.weininger@live.com>
2022-02-27 18:04:37 -0800

    Upload banner background image directly

--- libs/remotion/forms/src/lib/backgrounds/ImageBackground.form.tsx
+++ libs/remotion/forms/src/lib/backgrounds/ImageBackground.form.tsx
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
-import { Box, Button, ButtonGroup, FormControl, FormHelperText, FormLabel, HStack, IconButton, Input, Text, useBreakpoint, VStack } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, FormControl, FormHelperText, FormLabel, HStack, IconButton, Text, useBreakpoint, useDisclosure, useToast, VStack } from '@chakra-ui/react';
 import { BackgroundComponents } from '@pulsebanner/remotion/components';
 import { LayerForm } from '../LayerForm';
 import { StarIcon } from '@chakra-ui/icons';
+import { FileUploadModal } from '@pulsebanner/react-image-upload';
+
+const sampleImageMap: Record<string, string> = {
+    Sky: 'http://2.bp.blogspot.com/-QkeZcIm3X_g/VJRzwWkYfXI/AAAAAAAAJWg/HhIgNTT6PtM/s1600/clouds-1500x500-twitter-header-04.jpg',
+    Rainbow: 'https://www.designbolts.com/wp-content/uploads/2014/06/Colorful-twitter-header-banner.jpg',
+    Night: 'http://1.bp.blogspot.com/-K4otZmbNpuE/U5cZmaTVi8I/AAAAAAAAHZk/PSq3iuY9zdA/s1600/twitter-header-1500x500-nature+(1).jpg',
+};
 
 export const ImageBackground: LayerForm<typeof BackgroundComponents.ImageBackground> = ({ props, setProps, showPricing, accountLevel }) => {
-    const [currentString, setCurrentString] = useState(props.src ?? '');
-
-    const onClickPremium = () => {
-        showPricing(true);
-    };
-
+    const toast = useToast();
+    const { isOpen, onClose, onOpen } = useDisclosure();
     const breakpoint = useBreakpoint();
-
-    const sampleImageMap: Record<string, string> = {
-        Sky: 'http://2.bp.blogspot.com/-QkeZcIm3X_g/VJRzwWkYfXI/AAAAAAAAJWg/HhIgNTT6PtM/s1600/clouds-1500x500-twitter-header-04.jpg',
-        Rainbow: 'https://www.designbolts.com/wp-content/uploads/2014/06/Colorful-twitter-header-banner.jpg',
-        Night: 'http://1.bp.blogspot.com/-K4otZmbNpuE/U5cZmaTVi8I/AAAAAAAAHZk/PSq3iuY9zdA/s1600/twitter-header-1500x500-nature+(1).jpg',
-    };
 
     return (
         <Box w="full">
@@ -34,7 +30,7 @@ export const ImageBackground: LayerForm<typeof BackgroundComponents.ImageBackgro
                 </FormLabel>
                 <FormLabel>
                     <HStack>
-                        <Text>Background image URL</Text>
+                        <Text>Custom background image</Text>
                         <Box>
                             {breakpoint === 'base' && (
                                 <IconButton w="min" aria-label="Premium" icon={<StarIcon />} colorScheme="teal" variant="ghost" onClick={() => showPricing(true)} />
@@ -47,21 +43,22 @@ export const ImageBackground: LayerForm<typeof BackgroundComponents.ImageBackgro
                         </Box>
                     </HStack>
                 </FormLabel>
-                <Input
-                    disabled={accountLevel === 'Free' ? true : false}
-                    title={accountLevel === 'Free' ? 'Unlock with premium' : ''}
-                    w="full"
-                    type="url"
-                    defaultValue={props.src ?? ''}
-                    onChange={(e) => {
-                        setCurrentString(e.target.value);
-                        setProps({ ...(props ?? {}), src: e.target.value && e.target.value !== '' ? e.target.value : undefined });
+                <Button onClick={accountLevel === 'Free' ? () => showPricing(true) : onOpen}>Upload image</Button>
+                <FileUploadModal
+                    onUpload={(url) => {
+                        setProps({ ...(props ?? {}), src: `${url}?r=${Date.now().toString()}` });
+                        toast({
+                            status: 'success',
+                            title: 'Background image uploaded',
+                            position: 'top',
+                        });
                     }}
+                    url="/api/features/banner/upload?folder=backgrounds"
+                    isOpen={isOpen}
+                    onClose={onClose}
+                    title="Upload banner background"
                 />
-                <Button disabled={accountLevel === 'Free' ? true : false} onClick={() => setProps({ ...(props ?? {}), src: currentString !== '' ? currentString : undefined })}>
-                    Set Image URL
-                </Button>
-                <FormHelperText>Enter a URL to an image to use. Image size should be exactly 1500x500 for best appearance.</FormHelperText>
+                <FormHelperText>Image size should be exactly 1500x500 for best appearance.</FormHelperText>
             </FormControl>
         </Box>
     );
