6ef4416856c346d7c81ce74182e564a8913090ab
alexweininger <alex.weininger@live.com>
2022-03-05 17:25:24 -0800

    improve discord webhook

--- apps/next/pages/api/stripe/webhook.ts
+++ apps/next/pages/api/stripe/webhook.ts
@@ -322,37 +322,56 @@ handler.post(async (req, res) => {
                     const userId = subscriptionInfo === null ? null : subscriptionInfo.userId;
                     // send webhook to discord saying someone subscribed
                     if (userId) {
-                        const priceId = subscriptionInfo.priceId;
-                        const priceInfo = await prisma.price.findUnique({
-                            where: {
-                                id: priceId,
-                            },
-                        });
-                        if (priceInfo !== null) {
-                            const intervalId = priceInfo.interval;
-                            const productId = priceInfo.productId;
+                        const notify = async () => {
 
-                            const productInfo = await prisma.product.findUnique({
+                            const user = await prisma.user.findUnique({
                                 where: {
-                                    id: productId,
-                                },
-                                select: {
-                                    name: true,
+                                    id: userId
                                 },
                             });
 
-                            if (productInfo !== null) {
-                                logger.info(`User successfully signed up for a membership: ${productInfo.name} - ${intervalId}. User: ${userId}`, { userId: userId });
+                            const priceId = subscriptionInfo.priceId;
+                            const priceInfo = await prisma.price.findUnique({
+                                where: {
+                                    id: priceId,
+                                },
+                            });
+                            if (priceInfo !== null) {
+                                const intervalId = priceInfo.interval;
+                                const productId = priceInfo.productId;
+
+                                const productInfo = await prisma.product.findUnique({
+                                    where: {
+                                        id: productId,
+                                    },
+                                    select: {
+                                        name: true,
+                                    },
+                                });
+
+                                if (productInfo !== null) {
+                                    const msg = `${user.name} upgraded to a ${intervalId}ly ${productInfo.name} subscription.`;
+                                    logger.info(msg, { userId: userId });
+                                    const count = await prisma.subscription.count({
+                                        where: {
+                                            status: {
+                                                in: ['active', 'past_due']
+                                            }
+                                        }
+                                    });
+
+                                    sendMessage(`${msg} Total premium users: ${count}`, process.env.DISCORD_NEW_SUBSCRIBER_URL);
+                                }
+                            } else {
+                                logger.info(`User successfully signed up for a membership. User: ${userId}`, { userId: userId });
                                 prisma.subscription.count().then((value) => {
                                     sendMessage(`"${userId}" signed up for a premium plan! Total premium users: ${value}`, process.env.DISCORD_NEW_SUBSCRIBER_URL);
                                 });
                             }
-                        } else {
-                            logger.info(`User successfully signed up for a membership. User: ${userId}`, { userId: userId });
-                            prisma.subscription.count().then((value) => {
-                                sendMessage(`"${userId}" signed up for a premium plan! Total premium users: ${value}`, process.env.DISCORD_NEW_SUBSCRIBER_URL);
-                            });
                         }
+
+                        // don't await this async method, it will finish in the background
+                        void notify();
                     }
                     break;
                 }
