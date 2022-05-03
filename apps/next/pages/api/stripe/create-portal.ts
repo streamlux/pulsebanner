aaa4cd4b6dafdc2c2016415e5084d4a0ef706f14
Alex Weininger <alex.weininger@live.com>
2022-05-03 00:17:29 -0700

    Update billing portal config

--- apps/next/pages/api/stripe/create-portal.ts
+++ apps/next/pages/api/stripe/create-portal.ts
@@ -35,7 +35,9 @@ handler.post(async (req, res) => {
     return res.status(200).json(body);
 });
 
+
 async function createBillingPortalConfiguration(): Promise<Stripe.BillingPortal.Configuration> {
+
     return await stripe.billingPortal.configurations.create({
         business_profile: {
             headline: 'PulseBanner',
@@ -44,25 +46,62 @@ async function createBillingPortalConfiguration(): Promise<Stripe.BillingPortal.
         },
         features: {
             payment_method_update: { enabled: true },
+            subscription_cancel: {
+                enabled: true,
+                cancellation_reason: {
+                    enabled: true,
+                    options: [
+                        'missing_features',
+                        'too_expensive',
+                        'too_complex',
+                        'other',
+                    ]
+                }
+            },
+            customer_update: {
+                enabled: true,
+                allowed_updates: ['email']
+            },
             invoice_history: {
                 enabled: true,
             },
             subscription_update: {
                 default_allowed_updates: ['price'],
                 enabled: true,
-                products: [
-                    {
-                        // monthly and yearly personal
-                        product: 'prod_J1kovG9t6kOvJ1',
-                        prices: ['price_1JwkpXJzF2VT0EeKXFGDsr5A', 'price_1JwgTKJzF2VT0EeKxAbRNX6d']
-                    },
-                    {
-                        // monthly and yearly professional
-                        product: 'prod_J1kpUjbr9VENK1',
-                        prices: ['price_1JwgPBJzF2VT0EeK31OQd0UG', 'price_1JwgShJzF2VT0EeKsIvKjFDc']
-                    }
-                ]
+                products: await getProducts()
             },
+        }
+    });
+}
+
+async function getProducts(): Promise<Stripe.BillingPortal.ConfigurationCreateParams.Features.SubscriptionUpdate.Product[]> {
+    const products = await prisma.product.findMany({
+        where: {
+            AND: [
+                { active: true }
+            ],
+            prices: {
+                some: {
+                    type: 'recurring',
+                }
+            }
+        },
+        include: {
+            prices: {
+                where: {
+                    AND: [
+                        { active: true },
+                        { type: 'recurring' }
+                    ],
+                }
+            }
+        }
+    });
+
+    return products.map(product => {
+        return {
+            product: product.id,
+            prices: product.prices.map(price => price.id)
         }
     });
 }
